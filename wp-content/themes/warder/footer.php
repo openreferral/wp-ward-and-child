e4b82adc2a32a4086173077b64ed00595da40383
Sameer Siruguri <sameer@dstrategies.org>
2014-10-08 23:58:33 +0000

    added creative common license; had to change styling a bit to accom that

--- wp-content/themes/warder/footer.php
+++ wp-content/themes/warder/footer.php
@@ -27,10 +27,17 @@ $bavotasan_theme_options = bavotasan_theme_options();
 		</div><!-- .row -->
 
 		<div class="row">
-			<div class="col-lg-12">
+			<div class="col-lg-6">
 				<?php $class = ( is_active_sidebar( 'extended-footer' ) ) ? ' active' : ''; ?>
-				<span class="line<?php echo $class; ?>"></span>
-				<span class="pull-left">Copyright &copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a>. All Rights Reserved.</span>
+				<span >Copyright &copy; <?php echo date( 'Y' ); ?>&nbsp; </span> 
+				<span >
+				  <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+				    <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" />
+				  </a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>
+				</span>
+			</div>
+
+			<div class='col-lg-6'>
 				<span class="credit-link pull-right"><i class="icon-leaf"></i><?php printf( __( 'Designed by %s.', 'ward' ), '<a href="https://themes.bavotasan.com/2013/ward/">bavotasan.com</a>'); printf( __('Tweaked and hosted by %s.', 'ward'), '<a href="http://www.dstrategies.org/">Digital Strategies</a> and <a href="http://www.sarapis.org/">Sarapis</a>'); ?></span>
 			</div><!-- .col-lg-12 -->
 		</div><!-- .row -->
